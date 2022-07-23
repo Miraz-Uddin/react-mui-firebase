231af72510a5e1c14cd81b2d74c1f8782c4abfa9
Miraz Vai <freelancer.miraz.uddin@gmail.com>
2022-07-23 06:47:41 +0600

    Add a Single Document from Firestore

--- src/utils/firebase.config.js
+++ src/utils/firebase.config.js
@@ -1,7 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 // import { getFirestore } from "firebase/firestore";
-import { doc, getDoc, getFirestore } from "firebase/firestore";
+// import { doc, getDoc, getFirestore } from "firebase/firestore";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDtY5MoByr34wbg9M3HE48-YJgkhewFD_c",
@@ -20,10 +21,25 @@ export const auth = getAuth(app);
 const db = getFirestore(app);
 
 // Read a Single Document
-const docRef = doc(db, "blogs", "S20vOKay9KFPlREJRI4w");
-const docSnap = await getDoc(docRef);
-if (docSnap.exists()) {
-  console.log("Document data:", docSnap.data());
-} else {
-  console.log("No such document!");
+// const docRef = doc(db, "blogs", "S20vOKay9KFPlREJRI4w");
+// const docSnap = await getDoc(docRef);
+// if (docSnap.exists()) {
+//   console.log("Document data:", docSnap.data());
+// } else {
+//   console.log("No such document!");
+// }
+
+// Add a Single Document
+try {
+  const docRef = await addDoc(collection(db, "blogs"), {
+    description: "This is a Test Blog 4",
+    title: "Blog 4",
+    user: {
+      id: "9f7dCQN4MLSreF6NShw5godG1t03",
+      name: "Miraz",
+    },
+  });
+  console.log("Document written with ID: ", docRef.id);
+} catch (e) {
+  console.error("Error adding document: ", e);
 }
