e37a42f3a54afd3a9487115b75a58a26475b5db3
Miraz Vai <freelancer.miraz.uddin@gmail.com>
2022-11-21 15:54:02 +0600

    Blog Data Display on Real time Tested

--- src/blog/Blogs.jsx
+++ src/blog/Blogs.jsx
@@ -3,64 +3,51 @@ import React, { useContext, useEffect, useState } from "react";
 import { Grid } from "@mui/material";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, onSnapshot } from "firebase/firestore";
 import { AuthContext } from "../context/Auth.context";
 import { db } from "../utils/firebase.config";
 import Blog from "./Blog";
 
 export default function Blogs() {
   const [blogList, setBlogList] = useState([]);
-
+  
   const loadBlogs = async () => {
+    const colRef = collection(db, "blogs")
     try {
-      let allBlogs = [];
-      const querySnapshot = await getDocs(collection(db, "blogs"));
+      const allBlogs = [];
+      // Normal Data Fetching
+      const querySnapshot = await getDocs(colRef);
       querySnapshot.forEach((doc) => {
         const blog = doc.data();
-        allBlogs.push(blog);
+        allBlogs.push({...blog,id:doc.id});
       });
+      // Real Time Data Fetching
+      // await onSnapshot(colRef,(ss) => {
+      //     ss.forEach((doc) => {
+      //     allBlogs.push({...doc.data(),id:doc.id});
+      //   });
+      // })
       setBlogList(allBlogs);
     } catch (e) {
+      console.log(e)
       console.log("Error Occured while Blog loading from firestore");
     }
   };
 
   useEffect(() => {
     loadBlogs();
-  }, []);
-  console.log(blogList);
-
-  const { loading, currentUser } = useContext(AuthContext);
-  //   console.log(loading, currentUser);
-  if (loading && currentUser) {
-    // Total Blogs When logged in Scope
-    return (
-      <>
-        <Container component="main">
-          <CssBaseline />
-          <Grid container spacing={0.5} style={{ marginTop: "30px" }}>
-            {blogList.length != 0 &&
-              blogList.map((blog, i) => {
-                return <Blog blog={blog} key={i} />;
-              })}
-          </Grid>
-        </Container>
-      </>
-    );
-  } else {
-    // Total Blogs When logged Out Scope
-    return (
-      <>
-        <Container component="main">
-          <CssBaseline />
-          <Grid container spacing={0.5} style={{ marginTop: "30px" }}>
-            {blogList.length != 0 &&
-              blogList.map((blog, i) => {
-                return <Blog blog={blog} key={i} index={i} />;
-              })}
-          </Grid>
-        </Container>
-      </>
-    );
-  }
+  }, [])
+  return (
+    <>
+      <Container component="main">
+        <CssBaseline />
+        <Grid container spacing={0.5} style={{ marginTop: "30px" }}>
+          {blogList.length != 0 &&
+            blogList.map((blog, i) => {
+              return <Blog blog={blog} key={i} index={i} />;
+            })}
+        </Grid>
+      </Container>
+    </>
+  );
 }
